4c1e955b4d994a769047066ed77dc2f3e62f14e0
s-ewing <samuel.ewing@outlook.com>
2021-07-29 20:03:00 -0500

    added Astronaut.ts, Cargo.ts, Rocket.ts

---
+++ studio/Rocket.js
@@ -0,0 +1,41 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var totalMassKg = 0;
+        items.forEach(function (item) { return totalMassKg += item.massKg; });
+        return totalMassKg;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        return false;
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
